33f2ecfc2636bc287ce64d475055c46a4e410138
Elisej0072 <Elise22x08@gmail.com>
2026-01-27 12:31:31 -0500

    1

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
- 
+import './style.css'
+const numbers = [ one, two, three, four, five, six]
+
+function roll () {
+    const container = document.querySelector(".container");
+    const html = 
+    
+}
