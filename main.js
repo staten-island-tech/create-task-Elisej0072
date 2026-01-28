4911756953b93e9de646d5ccba52fd852d253e95
Elisej0072 <Elise22x08@gmail.com>
2026-01-28 12:32:14 -0500

    2

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-import './style.css'
-const numbers = [ one, two, three, four, five, six]
+const numbers = [ "one", "two", "three", "four", "five", "six"]
 
-function roll () {
-    const container = document.querySelector(".container");
-    const html = 
-    
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
+console.log(getRandomInt(6));
+
+
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+// function results() {
+// if(numbers ===  ){
+//     console.log();
+// } 
+//  else if () {
+//     console.log();
+// } else {
+//     console.log()
+// }
+// if () {
+//     console.log();
+// } else if () {
+//     console.log();
+// } else {
+//     console.log();
+// }}
