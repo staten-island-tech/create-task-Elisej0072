a7decee3ac8ad400a43159a8d360dda68867b104
Elisej0072 <Elise22x08@gmail.com>
2026-01-29 12:31:07 -0500

    3?

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-const numbers = [ "one", "two", "three", "four", "five", "six"]
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-console.log(getRandomInt(6));
+// const numbers = [1, 2, 3, 4, 5, 6];
 
 
+// function getRandomInt(min, max) {
+//   const minCeiled = Math.ceil(min);
+//   const maxFloored = Math.floor(max);
+//   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+// }
 
-function getRandomIntInclusive(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-}
+// console.log(getRandomInt(1, 7));
 
-// function results() {
-// if(numbers ===  ){
+
+
+// function roll() {
+// let result = 0;
+// if( ){
 //     console.log();
 // } 
 //  else if () {
@@ -29,3 +28,15 @@ function getRandomIntInclusive(min, max) {
 // } else {
 //     console.log();
 // }}
+
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
+// console.log(getRandomInt(6));
+
+
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const x = numbers.filter((numbers) => numbers % 3 === 0);
+console.log(x);
+numbers.filter((numbers) => numbers % 3  === 0).forEach((el) => console.log(el));
