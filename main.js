fa8039e53a8d4209aec66c645cf3c08b5172a6f5
Elisej0072 <Elise22x08@gmail.com>
2026-01-30 12:30:00 -0500

    4

--- main.js
+++ main.js
@@ -1,42 +1,32 @@
-// const numbers = [1, 2, 3, 4, 5, 6];
+const numbers = [1, 2, 3, 4, 5, 6];
 
 
-// function getRandomInt(min, max) {
-//   const minCeiled = Math.ceil(min);
-//   const maxFloored = Math.floor(max);
-//   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-// }
 
-// console.log(getRandomInt(1, 7));
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
+}
 
-
+let result = console.log(getRandomInt(1, 7));
 
 // function roll() {
-// let result = 0;
-// if( ){
-//     console.log();
+// if(result === 1){
+// console.log(You rolled a one!);
 // } 
-//  else if () {
-//     console.log();
-// } else {
-//     console.log()
+// else if (result === 2) {
+// console.log(You rolled a two!!);
 // }
-// if () {
-//     console.log();
-// } else if () {
-//     console.log();
-// } else {
-//     console.log();
+// else if (result === 3) {
+// console.log(You rolled a three!);
+// }
+// else if (result === 4) {
+// console.log(You rolled a four!!);
+// }
+// else if (result === 5) {
+// console.log(You rolled a five!);
+// }
+// else if (result === 6) {
+// console.log(You rolled a six!);
 // }}
 
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * max);
-// }
-// console.log(getRandomInt(6));
-
-
-
-const numbers = [1, 2, 3, 4, 5, 6];
-const x = numbers.filter((numbers) => numbers % 3 === 0);
-console.log(x);
-numbers.filter((numbers) => numbers % 3  === 0).forEach((el) => console.log(el));
