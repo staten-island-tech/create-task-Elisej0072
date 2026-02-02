3c1cc70e586b556eca411937e21c00c548833b97
Elisej0072 <Elise22x08@gmail.com>
2026-02-02 12:31:23 -0500

    5

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const numbers = [1, 2, 3, 4, 5, 6];
-
-//"" for console log
+const result = []
 
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
@@ -8,25 +7,28 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
 }
 
-let result = console.log(getRandomInt(1, 7));
+console.log(getRandomInt(1, 7));
 
-// function roll() {
-// if(result === 1){
-// console.log(You rolled a one!);
-// } 
-// else if (result === 2) {
-// console.log(You rolled a two!!);
-// }
-// else if (result === 3) {
-// console.log(You rolled a three!);
-// }
-// else if (result === 4) {
-// console.log(You rolled a four!!);
-// }
-// else if (result === 5) {
-// console.log(You rolled a five!);
-// }
-// else if (result === 6) {
-// console.log(You rolled a six!);
-// }}
+function roll() {
+numbers.forEach((number) => result.push(number))
+console.log(results);
+
+if(result === 1){
+console.log("You rolled a one!");
+} 
+else if (result === 2) {
+console.log("You rolled a two!!");
+}
+else if (result === 3) {
+console.log("You rolled a three!");
+}
+else if (result === 4) {
+console.log("You rolled a four!!");
+}
+else if (result === 5) {
+console.log("You rolled a five!");
+}
+else if (result === 6) {
+console.log("You rolled a six!");
+}}
 
